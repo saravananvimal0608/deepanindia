75d1f41ca0ddebc227bfdca076ff3d79a3f38195
Saravanan Vimal <saravananvimal0608@gmail.com>
2026-03-19 15:15:18 +0530

    map updated

--- src/app/components/Footer/page.js
+++ src/app/components/Footer/page.js
@@ -279,7 +279,7 @@ export default function Footer() {
 
         <Box className={styles.mapContainer}>
           <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3885.6682900232054!2d80.29056200000001!3d13.120187999999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a52670a343aef1d%3A0x89acc915e36fd6e6!2sFirst%20Choice%20Investment!5e0!3m2!1sen!2sin!4v1748239450808!5m2!1sen!2sin"
+            src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3885.6676152711425!2d80.28788377507901!3d13.120230687208938!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMTPCsDA3JzEyLjgiTiA4MMKwMTcnMjUuNyJF!5e0!3m2!1sen!2sin!4v1773913194643!5m2!1sen!2sin"
             className={styles.mapIframe}
             allowFullScreen=""
             loading="lazy"
